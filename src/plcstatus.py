0acf20ac8f315ab543cfba962b67d9750b6b7d8b
maxmcdevitt1 <maxmcdevitt1@icloud.com>
2026-05-27 15:41:19 -0400

    f

---
+++ src/plcstatus.py
@@ -0,0 +1,72 @@
+from time import sleep
+import os
+import csv
+from datetime import datetime
+
+from plc_client import Plc, COILS
+
+plc = Plc()
+
+colors = {
+    # Terminal colors
+    "RESET":"\033[0m",
+    "BOLD":"\033[1m",
+    "GREEN":"\033[92m",
+    "RED":"\033[91m",
+    "YELLOW":"\033[93m",
+    "BLUE":"\033[94m",
+    "CYAN":"\033[96m",
+    "GRAY":"\033[90m",
+}
+
+def on_off(coil):
+    if coil:
+        return(f"{colors['GREEN']}ON{colors['RESET']}")
+    return(f"{colors['RED']}OFF{colors['RESET']}")
+
+def print_status(status):
+    os.system("clear")
+    print(f"{colors['BOLD']}{colors['GREEN']}Connected to PLC")
+
+    for name in COILS:
+        print(f"{name:<18}{on_off(status[name])}")
+
+    print(f"{'PART COUNT':<18}{colors['BLUE']}{status['parts']}")
+    print("_"*32)
+    print(f"{'MACHINE STATE':<18}{colors['BLUE']}{status['MACHINE STATE']}")
+
+def write_csv_header(writer):
+    writer.writerow([
+        "Time stamp",
+        "Part Count",
+        *COILS.keys(),
+        "Machine State",
+    ])
+
+def write_csv_row(writer, status):
+    writer.writerow([
+            datetime.now().isoformat(timespec="seconds"),
+            status["PARTS"],
+            *[status[name] for name in COILS],
+            status['MACHINE STATE'],
+    ])
+
+
+if not plc.connect():
+    print(f"{colors['RED']}Unable to connect to PLC.")
+    time.sleep(2)
+    raise SystemExit
+
+try:
+    os.makedirs("plc_logs", exist_ok=True)
+    with open("plc_logs/plc_data.csv", "a", newline="") as f:
+        writer = csv.writer(f)
+        write_csv_header(writer)
+
+        while True:
+            status = plc.read_status()
+            print_status(status)
+            write_csv_row(writer, status)
+            f.flush()
+            sleep(.2)
+finally: plc.close()
